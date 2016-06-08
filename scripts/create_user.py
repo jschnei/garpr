54607972f5d544acd30053ad041bb535208f6885
Jon Schneider <jschneider2013@gmail.com>
2016-06-08 18:04:22 -0400

    check regions when adding user

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -10,8 +10,9 @@ from pymongo import MongoClient, DESCENDING
 from bson.objectid import ObjectId
 
 from config.config import Config
-from model import User
-from dao import USERS_COLLECTION_NAME, DATABASE_NAME, ITERATION_COUNT
+from model import User, Region
+from dao import DATABASE_NAME, ITERATION_COUNT, \
+                REGIONS_COLLECTION_NAME, USERS_COLLECTION_NAME
 
 
 def create_user():
@@ -19,14 +20,25 @@ def create_user():
         print "incorrect number of arguments!"
         print "usage: python create_user.py username password region1 [region2] [region3]...."
         return
-        
+
     username = sys.argv[1]
     password = sys.argv[2]
     regions =  sys.argv[3:]
     config = Config()
     mongo_client = MongoClient(host=config.get_mongo_url())
 
-    #TODO: validate regions all exist
+    regions_col = mongo_client[DATABASE_NAME][REGIONS_COLLECTION_NAME]
+    valid_regions = [Region.from_json(region).id for region in regions_col.find()]
+    print valid_regions
+
+    for region in regions:
+        if region not in valid_regions:
+            print 'Invalid region name:', region
+
+    regions = [region for region in regions if region in valid_regions]
+    if len(regions) == 0:
+        print 'No valid region for new user'
+        return
 
     # more bytes of randomness? i think 16 bytes is sufficient for a salt
     salt = base64.b64encode(os.urandom(16))
